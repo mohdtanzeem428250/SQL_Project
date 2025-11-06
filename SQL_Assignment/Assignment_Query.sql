2f6e1b293d65d0ca8edb2c9377bae11844a64ded
Mohd Tanzeem <mohdtanzeem169@gmail.com>
2025-11-06 14:52:14 +0530

    Update Assignment_Query.sql

--- SQL_Assignment/Assignment_Query.sql
+++ SQL_Assignment/Assignment_Query.sql
@@ -1,1 +1,15 @@
+-- Table structure for table `books`
+--
 
+CREATE TABLE `books` (
+  `Book_ID` bigint(20) UNSIGNED NOT NULL,
+  `Title` varchar(100) DEFAULT NULL,
+  `Author` varchar(100) DEFAULT NULL,
+  `Genre` varchar(50) DEFAULT NULL,
+  `Published_Year` int(11) DEFAULT NULL,
+  `Price` decimal(10,2) DEFAULT NULL,
+  `Stock` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- 
